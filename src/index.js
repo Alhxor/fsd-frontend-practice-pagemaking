7360188cd1efcdfe8fbfb101a5e74bc79a346b59
Sergei Korchemkin <hellk.pub@gmail.com>
2020-10-27 02:30:22 +0300

    Small changes / code maintenance

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./index.scss";
 import "@/components/dropdown/dropdown";
 import "@/components/masked-input/masked-input";
 import "@/components/date-picker/date-picker";
+import "@/components/date-dropdown/date-dropdown";
