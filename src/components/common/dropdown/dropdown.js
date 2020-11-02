bfbe5432bd1ce6454b8667c4039ece8d22722968
Sergei Korchemkin <hellk.pub@gmail.com>
2020-11-02 06:24:04 +0300

    dropdown: fix expand / collapse issues, add 'separate' flag

--- src/components/common/dropdown/dropdown.js
+++ src/components/common/dropdown/dropdown.js
@@ -1,4 +1,4 @@
-import { subscribe } from "@/util/events";
+import { subscribe, dispatch } from "@/util/events";
 
 document.addEventListener("DOMContentLoaded", () => {
   const dropdowns = document.querySelectorAll(".dropdown");
@@ -6,38 +6,53 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function Dropdown(node) {
-  const id = node.id;
+  // const id = node.id;
   const text = node.querySelector(".dropdown__text");
   const defaultText = text.textContent;
   const dataKey = node.dataset.datakey;
-  // const content = document.querySelector(`#${id}+.dropdown__content`);
-  const content = document.querySelector(`#${node.dataset.contentid}`)
+  const content = document.querySelector(`#${node.dataset.contentid}`);
+  const isSeparate = content.classList.contains("dropdown__content--separate");
+  let isOpen = false;
 
   subscribe(dataKey + "/updateText", (payload) => {
     if (payload.text) text.textContent = payload.text;
     else text.textContent = defaultText;
   });
 
-  subscribe(dataKey + "/close", (payload) => {
+  subscribe(dataKey + "/close", () => {
     close();
+    isOpen = false;
+  });
+
+  subscribe(dataKey + "/open", () => {
+    open();
+    isOpen = true;
   });
 
   // dropdown expand / collapse
   const toggle = () => {
-    node.classList.toggle("dropdown--expanded");
-    content.classList.toggle("hidden");
+    if (isOpen) dispatch(dataKey + "/close");
+    else dispatch(dataKey + "/open");
+  };
+
+  const open = () => {
+    if (!isSeparate) node.classList.add("dropdown--sharpen-bottom-border");
+    node.classList.add("dropdown--highlight-border");
+    content.classList.remove("hidden");
   };
 
   const close = () => {
-    node.classList.remove("dropdown--expanded");
+    node.classList.remove("dropdown--highlight-border");
+    node.classList.remove("dropdown--sharpen-bottom-border");
     content.classList.add("hidden");
   };
 
   node.addEventListener("click", () => toggle());
 
   // collapse when user clicks the page
-  // [!] this is adding a new document listener for EVERY dropdown, needs rewrite
-  // document.addEventListener("click", ({ target }) => {
-  //   if (!node.contains(target) && !content.contains(target)) close();
-  // });
+  document.addEventListener("click", ({ target }) => {
+    const container = node.closest(".container-relative");
+    if (isOpen && !container.contains(target) && !content.contains(target))
+      dispatch(dataKey + "/close");
+  });
 }
