2b3f683c9fef58c9484d96e39e19299f41fa4001
Sergei Korchemkin <hellk.pub@gmail.com>
2020-11-05 22:20:52 +0300

    Add like-button component

---
+++ src/components/like-button/like-button.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const likeButtons = document.querySelectorAll(".like-button");
+  likeButtons.forEach((btn) => LikeButton(btn));
+});
+
+function LikeButton(node) {
+
+}
--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ import "@/components/masked-input/masked-input";
 import "@/components/date-picker/date-picker";
 import "@/components/date-dropdown/date-dropdown";
 import "@/components/filter-date-dropdown/filter-date-dropdown";
+import "@/components/like-button/like-button";
