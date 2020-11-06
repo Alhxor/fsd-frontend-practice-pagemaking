1ce30abc027dce9e4509404b2929b4cfd7332999
Sergei Korchemkin <hellk.pub@gmail.com>
2020-11-06 08:24:17 +0300

    like-button: Adjust text position to better match the design

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -8,19 +8,22 @@ function LikeButton(node) {
   const icon = node.querySelector(".like-button__icon .material-icons");
   let clicked = [...node.classList].includes("like-button--clicked");
 
+  const buildText = (likes) =>
+    likes < 10 ? String.fromCharCode(160) + likes : String(likes);
+
   const clickListener = () => {
     let likes = parseInt(counter.textContent);
 
     if (!clicked) {
-      counter.textContent = likes + 1;
+      counter.textContent = buildText(likes + 1);
       icon.textContent = "favorite";
     } else {
-      counter.textContent = likes - 1;
+      counter.textContent = buildText(likes - 1);
       icon.textContent = "favorite_border";
     }
 
-    node.classList.toggle("like-button--clicked")
-    clicked = !clicked
+    node.classList.toggle("like-button--clicked");
+    clicked = !clicked;
   };
 
   node.addEventListener("click", clickListener);
