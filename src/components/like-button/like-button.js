c6a5c58a5205bcd183513e8de56e02baafbc729b
Sergei Korchemkin <hellk.pub@gmail.com>
2020-11-06 06:59:04 +0300

    like-button: add click functionality

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -4,5 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function LikeButton(node) {
+  const counter = node.querySelector(".like-button__counter");
+  const icon = node.querySelector(".like-button__icon .material-icons");
+  let clicked = [...node.classList].includes("like-button--clicked");
 
+  const clickListener = () => {
+    let likes = parseInt(counter.textContent);
+
+    if (!clicked) {
+      counter.textContent = likes + 1;
+      icon.textContent = "favorite";
+    } else {
+      counter.textContent = likes - 1;
+      icon.textContent = "favorite_border";
+    }
+
+    node.classList.toggle("like-button--clicked")
+    clicked = !clicked
+  };
+
+  node.addEventListener("click", clickListener);
 }
