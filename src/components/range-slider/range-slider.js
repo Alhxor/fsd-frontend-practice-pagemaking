2d03629a829bf1efebb16a61cc13103f64ce52fc
Sergei Korchemkin <hellk.pub@gmail.com>
2020-11-06 15:14:15 +0300

    range-slider: implement the rest of basic functionality

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -7,49 +7,70 @@ function RangeSlider(node) {
   const text = node.querySelector(".range-slider__text");
   const slider = node.querySelector(".range-slider__slider");
   const selection = slider.querySelector(".range-slider__selection");
-  const selectionStyles = getComputedStyle(selection);
   const start = selection.querySelector(".range-slider__start");
   const end = selection.querySelector(".range-slider__end");
   const { min, max, units } = node.dataset;
 
-  selection.style.left = "40px";
-  selection.style.right = "50px";
+  selection.style.left = "65px";
+  selection.style.right = "132px";
 
-  let left = parseInt(selectionStyles.left)
-  let right = parseInt(selectionStyles.right)
-  let width = parseInt(selectionStyles.width)
-  // console.log(slider.clientWidth - right - left)
+  const spacifyNumber = (n) => {
+    if (n > 999 && n < 1000000) {
+      let str = String(n)
+      return `${str.substr(0, str.length - 3)} ${str.substr(-3)}`
+    }
+    return n
+  };
 
-  let moveListener = () => {};
+  const updateText = () => {
+    let distanceFromLeft = (
+      parseInt(selection.style.left) / slider.clientWidth
+    ).toFixed(2);
+    let distanceFromRight = (
+      parseInt(selection.style.right) / slider.clientWidth
+    ).toFixed(2);
+
+    let unitsLeft = Math.trunc(parseInt(min) + max * distanceFromLeft);
+    let unitsRight = Math.trunc(max - max * distanceFromRight);
+
+    text.textContent = `${spacifyNumber(unitsLeft)}${units} - ${spacifyNumber(unitsRight)}${units}`;
+  };
+
+  updateText();
+
   let oldX = 0;
-
-  const createMoveListener = side => event => {
+  const createMoveListener = (side) => (event) => {
     if (oldX === 0) oldX = event.x;
 
-    const change = (side === "left") ? event.x - oldX : oldX - event.x
-    const current = parseInt(selection.style[side])
+    const change = side === "left" ? event.x - oldX : oldX - event.x;
+    oldX = event.x;
+    const current = parseInt(selection.style[side]);
+    let result = current + change;
 
-    selection.style[side] = current + change + "px"
-    oldX = event.x
-  }
+    if (result < 0) result = 0;
 
-  const upListener = () => {
-    oldX = 0;
-    document.removeEventListener("mousemove", moveListener);
-    document.removeEventListener("mouseup", upListener)
-  }
+    selection.style[side] = result + "px";
+    if (selection.clientWidth < 30) selection.style[side] = current + "px";
 
-  start.addEventListener("mousedown", () => {
-    moveListener = createMoveListener("left")
+    updateText();
+  };
 
-    document.addEventListener("mousemove", moveListener);
-    document.addEventListener("mouseup", upListener)
-  });
+  const moveListenerLeft = createMoveListener("left");
+  const moveListenerRight = createMoveListener("right");
 
-  end.addEventListener("mousedown", () => {
-    moveListener = createMoveListener("right")
+  const addListener = (btn, left = true) => {
+    btn.addEventListener("mousedown", () => {
+      const moveListener = left ? moveListenerLeft : moveListenerRight;
+      const upListener = () => {
+        oldX = 0;
+        document.removeEventListener("mousemove", moveListener);
+        document.removeEventListener("mouseup", upListener);
+      };
+      document.addEventListener("mousemove", moveListener);
+      document.addEventListener("mouseup", upListener);
+    });
+  };
 
-    document.addEventListener("mousemove", moveListener);
-    document.addEventListener("mouseup", upListener)
-  })
+  addListener(start);
+  addListener(end, false);
 }
