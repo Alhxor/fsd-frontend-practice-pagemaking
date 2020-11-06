6b121aad3cd67e6e6ce9b518f5b1d10984ccc8fd
Sergei Korchemkin <hellk.pub@gmail.com>
2020-11-06 07:47:03 +0300

    Add component rate-button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./index.scss";
-// import "@/components/common/dropdown/dropdown"
 import "@/components/options-dropdown/options-dropdown";
 import "@/components/masked-input/masked-input";
 import "@/components/date-picker/date-picker";
 import "@/components/date-dropdown/date-dropdown";
 import "@/components/filter-date-dropdown/filter-date-dropdown";
 import "@/components/like-button/like-button";
+import "@/components/rate-button/rate-button";
